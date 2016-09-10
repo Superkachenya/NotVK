eef271c1159a94068cedde0a8d7ce71982eddef0
Danil <voitenko.cr@gmail.com>
2016-09-10 16:26:41 +0300

    Initial Commit

---
+++ NotVK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotVK
+//
+//  Created by Danil on 9/10/16.
+//  Copyright © 2016 Cleveroad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
